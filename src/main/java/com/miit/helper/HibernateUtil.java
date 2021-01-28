93da8c6fa1cb10483fdcb7d77eaa4542811b4e49
siddp <siddp@DESKTOP-E3QJHPR.mshome.net>
2021-01-28 08:33:20 -0500

    Remove space

--- src/main/java/com/miit/helper/HibernateUtil.java
+++ src/main/java/com/miit/helper/HibernateUtil.java
@@ -4,7 +4,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-
 	private static SessionFactory sessionFactory = null;
 
 	// This Method Is Used To Create The Hibernate's SessionFactory Object
